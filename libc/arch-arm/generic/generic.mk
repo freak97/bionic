1c8da7c48e72fde193ced39ae19f88cde630174f
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2015-11-16 23:38:35 +0100

    Reimplement the generic version of strcat
    This version of strcat takes advantage of optimized versions of strcpy
    and strlen -- making it significantly faster on aarch64
    
    This is a forward-port of
    https://android-review.linaro.org/#/c/15553
    
    Change-Id: I44a9989cf2249f8306c9271d923675dd23a425cb
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- libc/arch-arm/generic/generic.mk
+++ libc/arch-arm/generic/generic.mk
@@ -4,6 +4,7 @@ libc_bionic_src_files_arm += \
     arch-arm/generic/bionic/strcmp.S \
     arch-arm/generic/bionic/strcpy.S \
     arch-arm/generic/bionic/strlen.c \
+    bionic/strcat.c \
     bionic/__strcat_chk.cpp \
     bionic/__strcpy_chk.cpp
 
@@ -19,4 +20,3 @@ endif
 libc_openbsd_src_files_arm += \
     upstream-openbsd/lib/libc/string/memmove.c \
     upstream-openbsd/lib/libc/string/stpcpy.c \
-    upstream-openbsd/lib/libc/string/strcat.c \
